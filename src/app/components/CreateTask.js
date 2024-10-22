ce5db3be4d6f159793d94a691cee014e69d6f24a
Lewis Youl <lewis.youl@trustflight.com>
2024-10-22 07:59:19 +0100

    fix: new task modal styling

--- src/app/components/CreateTask.js
+++ src/app/components/CreateTask.js
@@ -37,21 +37,23 @@ export default function CreateTask({ onCreate }) {
   }
 
   return (
-    <div className="bg-white mt-0 md:mt-8 w-screen md:w-[860px] rounded-md py-4 px-4">
-      <div className="max-w-6xl m-auto">
-        <h1 className="text-center text-2xl">New Task</h1>
+    <div className="pt-4 px-4 w-screen flex justify-center">
+      <div className="bg-white mt-0 md:mt-8 w-full md:w-[860px] rounded-md py-4 px-4">
+        <div className="max-w-6xl m-auto">
+          <h1 className="text-center text-2xl">New Task</h1>
 
-        <input autoFocus className="mt-4 border w-full rounded-sm px-2 py-1" onChange={(e => setTitle(e.target.value))} type="text" placeholder="What do you need to do?" />
-        {error && <div className="text-red-500">Please enter a title for your task</div>}
-      </div>
-      <div className="flex justify-end mt-4">
-        <button
-          onClick={saveTask}
-          type="button"
-          className="inline-flex rounded-md bg-palqrblue px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-palqrblue"
-        >
-          Save
-        </button>
+          <input autoFocus className="mt-4 border w-full rounded-sm px-2 py-1" onChange={(e => setTitle(e.target.value))} type="text" placeholder="What do you need to do?" />
+          {error && <div className="text-red-500">Please enter a title for your task</div>}
+        </div>
+        <div className="flex justify-end mt-4">
+          <button
+            onClick={saveTask}
+            type="button"
+            className="inline-flex rounded-md bg-palqrblue px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-palqrblue"
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/app/contexts/modalContext.js
+++ src/app/contexts/modalContext.js
@@ -17,16 +17,6 @@ const ModalProvider = ({children})=>{
       <ModalContext.Provider value={{ setModalContent }}>
         {modalContent && <div onClick={handleOutsideClick} className="fixed h-screen w-full bg-gray-500 bg-opacity-50 z-10 flex justify-center">
           <div ref={modalRef} className="h-[0px]">
-            <div className="absolute top-0 right-0 pt-3 pr-3 z-20">
-              <button
-                type="button"
-                className="inline-flex items-center justify-center text-gray-800 hover:text-gray-500"
-                onClick={() => setModalContent(null)}
-              >
-                <span className="sr-only">Close modal</span>
-                <XMarkIcon className="h-8 w-8" aria-hidden="true" />
-              </button>
-            </div>
             {modalContent}
           </div>
         </div>}
