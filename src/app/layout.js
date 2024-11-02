397c985407d0321e5e4f202b9d0faba6833492c8
Lewis Youl <lewis.youl@trustflight.com>
2024-11-02 18:10:30 +0000

    chore: add google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Space_Grotesk, Gabarito } from 'next/font/google'
 import './globals.css'
 import dynamic from 'next/dynamic'
+import Script from 'next/script'
 
 const PageWrapper = dynamic(() => import('./components/PageWrapper'), {
   ssr: false
@@ -30,7 +31,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html className="h-full bg-white" lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TJTSX6H8');
+          `}
+        </Script>
+      </head>
       <body className={`h-full ${spaceGrotesk.variable} ${gabarito.variable} font-sans`}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJTSX6H8" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
+        </noscript>
+
         <PageWrapper>
           {children}
         </PageWrapper>
