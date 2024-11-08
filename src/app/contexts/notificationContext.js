ed45e459f070e9100fa41cd81c7e67358755cc42
Lewis Youl <lewis.youl@trustflight.com>
2024-11-08 20:24:22 +0000

    style: change notification style to match rest of app

--- src/app/contexts/notificationContext.js
+++ src/app/contexts/notificationContext.js
@@ -29,20 +29,19 @@ const NotificationProvider = ({children})=>{
   return (
       <NotificationContext.Provider value={{ setNotificationContent }}>
         {!!notificationContent && <>
-          {/* Global notification live region, render this permanently at the end of the document */}
           <div
             aria-live="assertive"
             className="z-50 pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
           >
             <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
-              <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition data-[closed]:data-[enter]:translate-y-2 data-[enter]:transform data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-100 data-[enter]:ease-out data-[leave]:ease-in data-[closed]:data-[enter]:sm:translate-x-2 data-[closed]:data-[enter]:sm:translate-y-0">
+              <div className="pointer-events-auto w-full max-w-sm overflow-hidden bg-gray-900 border border-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 transition data-[closed]:data-[enter]:translate-y-2 data-[enter]:transform data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-100 data-[enter]:ease-out data-[leave]:ease-in data-[closed]:data-[enter]:sm:translate-x-2 data-[closed]:data-[enter]:sm:translate-y-0">
                 <div className="p-4">
                   <div className="flex items-start">
                     <div className="flex-shrink-0">
                       <CheckCircleIcon aria-hidden="true" className="h-6 w-6 text-green-400" />
                     </div>
                     <div className="ml-3 w-0 flex-1 pt-0.5">
-                      <p className="text-sm font-medium text-gray-900">{notificationContent.title}</p>
+                      <p className="text-sm font-medium text-white">{notificationContent.title}</p>
                       <p className="mt-1 text-sm text-gray-500">{notificationContent.message}</p>
                     </div>
                     <div className="ml-4 flex flex-shrink-0">
@@ -51,7 +50,7 @@ const NotificationProvider = ({children})=>{
                         onClick={() => {
                           setNotificationContent(null)
                         }}
-                        className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        className="inline-flex bg-gray-900 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                       >
                         <span className="sr-only">Close</span>
                         <XMarkIcon aria-hidden="true" className="h-5 w-5" />
