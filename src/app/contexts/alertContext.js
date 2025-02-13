14766ad3a9de8197c7ac612113c2afc03aa2d805
Lewis Youl <lewis.youl@trustflight.com>
2025-02-13 18:16:16 +0000

    styling: update alert styling

--- src/app/contexts/alertContext.js
+++ src/app/contexts/alertContext.js
@@ -20,13 +20,13 @@ const AlertProvider = ({children})=>{
           <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
             <div className="flex z-50 min-h-full items-end justify-center p-4 text-center sm:items-start sm:p-0">
               <div
-                className="relative z-50 transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                className="relative z-50 transform overflow-hidden rounded-lg bg-gray-900 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
               >
                 <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
                   <button
                     type="button"
                     onClick={() => setAlertContent(null)}
-                    className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className="rounded-md bg-gray-900 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                   >
                     <span className="sr-only">Close</span>
                     <XMarkIcon aria-hidden="true" className="h-6 w-6" />
@@ -37,11 +37,11 @@ const AlertProvider = ({children})=>{
                     <ExclamationTriangleIcon aria-hidden="true" className="h-6 w-6 text-red-600" />
                   </div>
                   <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <div as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                    <div as="h3" className="text-base font-semibold leading-6 text-gray-100">
                       {alertContent.title}
                     </div>
                     <div className="mt-2">
-                      <p className="text-sm text-gray-500">
+                      <p className="text-sm text-gray-300">
                         {alertContent.message}
                       </p>
                     </div>
@@ -58,7 +58,7 @@ const AlertProvider = ({children})=>{
                   <button
                     type="button"
                     onClick={() => setAlertContent(null)}
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-200 sm:mt-0 sm:w-auto"
                   >
                     {alertContent.cancelText}
                   </button>
